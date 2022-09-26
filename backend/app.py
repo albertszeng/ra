94870804343e2fce1a29124292f9bae961cf388c
Luis Perez <luis.perez.live@gmail.com>
2022-09-25 23:20:00 -0700

    fix type errors

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ from flask import request
 from sqlalchemy.ext import mutable
 
 
-from typing import Dict
+from typing import Dict, TypedDict, Union
 
 
 app = flask.Flask(__name__)
@@ -24,19 +24,20 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 class RaGame(mutable.Mutable, ra.RaGame):
     """Required so database can update on changes to state."""
 
-    def __getstate__(self):
+    def __getstate__(self) -> Dict[str, str]:
         d = self.__dict__.copy()
         d.pop('_parents', None)
         return d
 
 
-class Game(db.Model):
+class Game(db.Model):  # pyre-ignore[11]
     """Database used to persist games across requests. """
     # We use the uuid.hex property, which is 32-char string.
-    id = db.Column(db.String(32), primary_key=True)
+    # pyre-ignore[11]
+    id: db.Column = db.Column(db.String(32), primary_key=True)
 
     # This is a pickle-d version of the game so we can restore state.
-    data = db.Column(db.PickleType, nullable=False)
+    data: db.Column = db.Column(db.PickleType, nullable=False)
 
 
 # Creates the database and tables as specified by all db.Model classes.
@@ -45,8 +46,11 @@ db.create_all()
 
 def get_game_repr(game: ra.RaGame) -> str:
     legal_actions = game.get_possible_actions()
+    val = str(game.game_state)
+    if not legal_actions:
+        return val
     prompt = game.get_action_prompt(legal_actions, helpful_prompt=True)
-    return f"{game.game_state}\n\n{prompt}"
+    return f"{val}\n\n{prompt}"
 
 
 @app.route("/", methods=["GET"])
@@ -54,11 +58,23 @@ def hello_world() -> str:
     return "<p>Hello, World!</p>"
 
 
+class Message(TypedDict):
+    message: str
+
+
+class ActResponse(TypedDict):
+    gameState: str
+
+
+class StartResponse(ActResponse):
+    gameId: uuid.UUID
+
+
 @app.route("/start", methods=["POST"])
-def start():
+def start() -> Union[Message, StartResponse]:
     gameId = uuid.uuid4()
     if not (players := request.json.get("player_names")) or len(players) < 2:
-        return {'message': 'Cannot start game. Need player names.'}
+        return Message(message='Cannot start game. Need player names.')
 
     game = ra.RaGame(
         player_names=players,
@@ -67,60 +83,53 @@ def start():
     game.init_game()
 
     # Add game to database.
-    dbGame = Game(id=gameId.hex, data=game)
+    dbGame = Game(id=gameId.hex, data=game)  # pyre-ignore[28]
     db.session.add(dbGame)
     db.session.commit()
 
-    return {
-        'gameId': gameId,
-        'gameState': get_game_repr(game),
-    }
+    return StartResponse(gameId=gameId, gameState=get_game_repr(game))
 
 
 @app.route("/delete", methods=["POST"])
-def delete():
+def delete() -> Message:
     if not (gameId := request.json.get('gameId')):
-        return {'messsage': 'Invalid request.'}
+        return Message(message='Invalid request.')
 
     gameId = uuid.UUID(gameId)
     if not (dbGame := db.session.get(Game, gameId.hex)):
-        return {'messsage': f'No game with id {gameId} found.'}
+        return Message(message=f'No game with id {gameId} found.')
 
     db.session.delete(dbGame)
     db.session.commit()
-    return {
-        'message': f'Deleted game: {gameId}',
-    }
+    return Message(message=f'Deleted game: {gameId}')
 
 
 @app.route("/action", methods=["POST"])
-def action():
+def action() -> Union[Message, ActResponse]:
     _SERVER_ACTIONS = ['LOAD']
     if not (gameId := request.json.get('gameId')):
-        return {'message': f'Cannot act on non-existing game: {gameId}'}
+        return Message(message=f'Cannot act on non-existing game: {gameId}')
     gameId = uuid.UUID(gameId)
-    if not (action := request.json.get("command")):
-        return {'message': 'No action'}
-
+    action = request.json.get("command")
+    if not action:
+        return Message(message='No action')
 
     if action not in _SERVER_ACTIONS:
         action = ra.parse_action(action)
         if isinstance(action, str):
-            return {'message': f'Unrecognized action: {action}'}
+            return Message(message=f'Unrecognized action: {action}')
 
     if not (dbGame := db.session.get(Game, gameId.hex)):
-        return {'message': f'No active game with id: {gameId}'}
+        return Message(message=f'No active game with id: {gameId}')
     game = dbGame.data
     if game.game_state.is_game_ended() or action == 'LOAD':
-        return {
-            'gameState': get_game_repr(game),
-        }
+        return ActResponse(gameState=get_game_repr(game))
 
     legal_actions = game.get_possible_actions()
     if not legal_actions:
-        return {'message': 'Internal Error: No valid actions. '}
+        return Message(message='Internal Error: No valid actions. ')
     if action not in legal_actions:
-        return {'message': f'Only legal actions are: {legal_actions}'}
+        return Message(message=f'Only legal actions are: {legal_actions}')
     t = game.execute_action(action, legal_actions)
     dbGame.data = copy.copy(game)
     db.session.commit()
@@ -131,6 +140,4 @@ def action():
         else:
             outfile.write(f"{action}\n")
 
-    return {
-        'gameState': get_game_repr(game),
-    }
+    return ActResponse(gameState=get_game_repr(game))
