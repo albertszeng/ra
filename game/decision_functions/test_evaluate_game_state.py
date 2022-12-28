ba81afba209124b3ea1161c9ab0b78a296d115cc
Albert Zeng <albertz@fb.com>
2022-12-27 19:37:51 -0800

    fix test class name

--- game/decision_functions/test_evaluate_game_state.py
+++ game/decision_functions/test_evaluate_game_state.py
@@ -7,7 +7,7 @@ from game import state as gs
 from game.decision_functions import evaluate_game_state as e
 
 
-class RaTest(unittest.TestCase):
+class EvaluateGameStateTest(unittest.TestCase):
     def test_evaluate_game_state_no_auction_tiles(self) -> None:
         game = ra.RaGame(player_names=["P1", "P2"], randomize_play_order=False)
         game.init_game()
