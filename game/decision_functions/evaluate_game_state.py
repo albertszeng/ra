9bd7d10922d162d51ba44dd71e350b3b95bc7758
Albert Zeng <albertz@fb.com>
2022-12-29 10:32:18 -0800

    add tests for evaluate_game_state

--- game/decision_functions/evaluate_game_state.py
+++ game/decision_functions/evaluate_game_state.py
@@ -4,17 +4,74 @@ from game import info as gi
 from game import scoring_utils as scoring
 from game import state as gs
 
+SUN_MODIFIERS_2P: Mapping[int, float] = {
+    1: -2.0,
+    2: -1.5,
+    3: -1.0,
+    4: -0.5,
+    5: 0.0,
+    6: 0.5,
+    7: 1.0,
+    8: 1.5,
+    9: 2.0,
+}
+
+SUN_MODIFIERS_3P_AND_4P: Mapping[int, float] = {
+    1: -2.0,
+    2: -1.67,
+    3: -1.33,
+    4: -1.0,
+    5: -0.67,
+    6: -0.33,
+    7: 0.0,
+    8: 0.33,
+    9: 0.67,
+    10: 1.0,
+    11: 1.33,
+    12: 1.67,
+    13: 2.0,
+}
+
+SUN_MODIFIERS_5P: Mapping[int, float] = {
+    1: -2.0,
+    2: -1.625,
+    3: -1.375,
+    4: -1.125,
+    5: -0.875,
+    6: -0.625,
+    7: -0.375,
+    8: -0.125,
+    9: 0.125,
+    10: 0.375,
+    11: 0.625,
+    12: 0.875,
+    13: 1.125,
+    14: 1.375,
+    15: 1.625,
+    16: 2.0,
+}
+
+# Maps a number of players to the sun modifiers
+SUN_MODIFIER_MAPPING: Mapping[int, Mapping[int, float]] = {
+    2: SUN_MODIFIERS_2P,
+    3: SUN_MODIFIERS_3P_AND_4P,
+    4: SUN_MODIFIERS_3P_AND_4P,
+    5: SUN_MODIFIERS_5P,
+}
+
 
 def evaluate_game_state_no_auction_tiles(game_state: gs.GameState) -> Dict[str, float]:
     """
-    Given a gamestate, provide a "score" for each player's state. Does NOT factor in
+    Given a gamestate, provide a "valuation" for each player's state. Does NOT factor in
     auction tiles into evaluation, so this should only be called if there are no
     auction tiles.
 
-    The scores for each player are NOT the same as the actual points they have, nor
+    The valuations for each player are NOT the same as the actual points they have, nor
     is it an estimation for how many points they will have. The only intended
-    guarantee is that better states will have higher scores.
+    guarantee is that better states will have higher valuations.
     """
+    # TODO(albertz): If the game has ended, the valuation is simply the end-game points
+
     # TODO(albertz): actually implement this, by evaluating:
     # - Currently held tiles
     #    - Niles + Floods: need to value for future, based on ras remaining
@@ -40,3 +97,39 @@ def evaluate_game_state_no_auction_tiles(game_state: gs.GameState) -> Dict[str, 
         + unrealized_points[player_state.get_player_name()]
         for player_state in game_state.player_states
     }
+
+
+def value_usable_sun(game_state: gs.GameState) -> Dict[str, float]:
+    """
+    Returns the valuation of each player's remaining sun.
+    """
+    # TODO(albertz): rethink if we need to value usable sun differently if it's the final round
+
+    return {"a": 0.0}
+
+
+def value_of_unusable_sun(game_state: gs.GameState) -> Dict[str, float]:
+    """
+    Returns the valuation of each player's remaining sun. Does NOT
+    give a value to unusable suns in the final round.
+    """
+    if game_state.is_final_round():
+        # if final round, do not factor in unusable sun, because it's already taken into
+        # account by the unrealized points
+        return {name: 0.0 for name in game_state.get_player_names()}
+
+    sun_modifiers = SUN_MODIFIER_MAPPING[game_state.get_num_players()]
+    unusable_sun_valuations: Dict[str, float] = {}
+    for player_state in game_state.player_states:
+        player_name = player_state.get_player_name()
+        player_suns = player_state.get_unusable_sun()
+
+        unusable_sun_valuations[player_name] = sum(
+            [sun_modifiers[sun] for sun in player_suns]
+        )
+    return {"a": 0.0}
+
+
+def value_one_players_unusable_sun(unusable_sun: List[str], num_players) -> float:
+    sun_modifiers = SUN_MODIFIER_MAPPING[num_players]
+    return sum([sun_modifiers[sun] for sun in unusable_sun])
--- game/decision_functions/test_evaluate_game_state.py
+++ game/decision_functions/test_evaluate_game_state.py
@@ -24,6 +24,27 @@ class EvaluateGameStateTest(unittest.TestCase):
         evaluation2 = e.evaluate_game_state_no_auction_tiles(game.game_state)
         self.assertTrue(evaluation2["P1"] > evaluation2["P2"])
 
+    # def test_value_one_players_unusable_sun(self) -> None:
+    #     unusable_sun_0 = []
+    #     self.assertEqual(e.value_one_players_unusable_sun(unusable_sun_0, 3), 0.0)
+
+    #     unusable_sun_1 = [1]
+    #     self.assertEqual(e.value_one_players_unusable_sun(unusable_sun_1, 3), -2.0)
+
+    #     unusable_sun_2 = [1]
+    #     self.assertEqual(e.value_one_players_unusable_sun(unusable_sun_2, 5), -2.0)
+
+    #     unusable_sun_3 = [1, 2]
+    #     self.assertEqual(e.value_one_players_unusable_sun(unusable_sun_3, 5), -3.625)
+
+    #     unusable_sun_4 = [1, 2, 14]
+    #     self.assertEqual(e.value_one_players_unusable_sun(unusable_sun_4, 5), -2.25)
+
+    # def test_value_of_unusable_sun(self) -> None:
+    #     game_state = gs.GameState(["P1", "P2"])
+
+    #     ra.execute_action_internal(game_state, gi.AUCTION, None, gi.INDEX_OF_GOLD)  # P1
+
 
 if __name__ == "__main__":
     unittest.main()
